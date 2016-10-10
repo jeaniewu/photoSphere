368f5449513b373649eada5e22672cb754068b0a
Jeanie Suparman <jeanie.wu@hotmail.com>
2016-10-10 12:47:48 -0700

    added Proto3

---
+++ unity-cardboard-photosphere/Assets/SwitchTexture.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchTexture : MonoBehaviour {
+
+	public float TapDelay = 5.0f;
+	private float _tapCountdown;
+	private Texture2D[] textureArray;
+	public int currTexture = 0;
+
+	public GameObject sphere;
+
+	// Use this for initialization
+	void Start () {
+		textureArray = GetComponent<TextureLoader> ().imageBuffer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (GvrViewer.Instance.Triggered && _tapCountdown <= 0)
+		{
+			sphere.GetComponent<Renderer> ().material.mainTexture = textureArray[currTexture];
+			currTexture++;
+			_tapCountdown = TapDelay;
+		}
+		else
+		{
+			_tapCountdown -= Time.deltaTime;
+		}
+	
+	}
+}
---
+++ unity-cardboard-photosphere/Assets/TextureLoader.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextureLoader : MonoBehaviour {
+
+	string pathPrefix = @"file://";
+	string pathImageAssets;
+	string pathSmall = @"/Textures/";
+	string filename = @"Texture_";
+	string fileSuffix = @".jpg";
+
+	public Texture2D[] imageBuffer = new Texture2D[10];
+
+	// Use this for initialization
+	void Start () {
+		pathImageAssets = Application.dataPath;
+		StartCoroutine(loadImages());
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	private IEnumerator loadImages()
+	{
+
+		//create filename index suffix "001",...,"027" (could be "999" either)
+		for (int i = 0; i < 6; i++) {
+			string indexSuffix = "";
+			float logIdx = Mathf.Log10 (i + 1);
+			if (logIdx < 1.0)
+				indexSuffix += "00";
+			else if (logIdx < 2.0)
+				indexSuffix += "0";
+			indexSuffix += (i + 1);
+			string fullFilename = pathPrefix + pathImageAssets + pathSmall + filename + indexSuffix + fileSuffix;
+			Debug.Log (fullFilename);
+			WWW www = new WWW (fullFilename);
+			yield return www;
+
+			Texture2D texTmp = new Texture2D (1024, 1024, TextureFormat.DXT1, false);
+			//LoadImageIntoTexture compresses JPGs by DXT1 and PNGs by DXT5     
+			www.LoadImageIntoTexture (texTmp);
+			imageBuffer[i] = texTmp;
+		}
+	}
+}
