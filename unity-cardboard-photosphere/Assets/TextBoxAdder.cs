c957c4db460d6626796088483f7ff38e278f07aa
swooasd <kyle.swensson@gmail.com>
2016-11-18 15:42:13 -0800

    Made text cat into a new seperate prefab and fixed some small stuff like making text rotate with cat when it is made

--- unity-cardboard-photosphere/Assets/TextBoxAdder.cs
+++ unity-cardboard-photosphere/Assets/TextBoxAdder.cs
@@ -43,6 +43,8 @@ public class TextBoxAdder : MonoBehaviour {
             }
             currentTextBox = (GameObject)Instantiate(textBox, this.transform);
             currentTextBox.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 12f, this.transform.position.z);
+            currentTextBox.transform.rotation = transform.rotation;
+            currentTextBox.transform.Rotate(new Vector3(0, 90, 0));
             setTextOfCurrentTextBox(sentences[currentSentence]);
             TextboxController newCurrentTextBoxController = currentTextBox.GetComponent<TextboxController>();
             newCurrentTextBoxController.SetInvisible();
