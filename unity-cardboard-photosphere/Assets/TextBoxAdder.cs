1328cdd72d110b7c718701bb560454acaee58e34
swooasd <kyle.swensson@gmail.com>
2016-11-17 17:20:46 -0800

    added some text box stuff

---
+++ unity-cardboard-photosphere/Assets/TextBoxAdder.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextBoxAdder : MonoBehaviour {
+
+    public GameObject textBox;
+    private GameObject currentTextBox;
+    public string[] sentences;
+    private int currentSentence;
+	// Use this for initialization
+	void Start () {
+       
+        currentSentence = -1;
+        sentences = new string[3];
+        sentences[0] = "sentence1";
+        sentences[1] = "2 beep boop";
+        sentences[2] = "last sentence";
+	}
+
+    void setTextOfCurrentTextBox(string newText)
+    {
+        Transform boxTextTransform = currentTextBox.transform.GetChild(1);
+        TextMesh boxTextTextMesh = boxTextTransform.GetComponent<TextMesh>();
+        boxTextTextMesh.text = newText;
+    }
+
+    void gotoNextSentence()
+    {
+        currentSentence++;
+        if (currentSentence > sentences.Length - 1)
+        {
+            currentSentence = -1;
+            TextboxController currentTextBoxController = currentTextBox.GetComponent<TextboxController>();
+            currentTextBoxController.FadeOutTextBox();
+        } else
+        {
+            if (currentSentence == 0)
+            {
+                currentTextBox = (GameObject)Instantiate(textBox, this.transform);
+                currentTextBox.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 12f, this.transform.position.z);
+            }
+
+            TextboxController currentTextBoxController = currentTextBox.GetComponent<TextboxController>();
+            currentTextBoxController.FadeOutTextBox();
+            currentTextBox = (GameObject)Instantiate(textBox, this.transform);
+            currentTextBox.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 12f, this.transform.position.z);
+            setTextOfCurrentTextBox(sentences[currentSentence]);
+            TextboxController newCurrentTextBoxController = currentTextBox.GetComponent<TextboxController>();
+            newCurrentTextBoxController.SetInvisible();
+            newCurrentTextBoxController.FadeInTextBox();
+
+
+        }
+    }
+    
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.X) == true)
+        {
+            gotoNextSentence();
+        }
+
+    }
+}
---
+++ unity-cardboard-photosphere/Assets/TextboxController.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TextboxController : MonoBehaviour {
+
+
+    bool isFadingOut;
+    bool isFadingIn;
+    int fadingOutTime;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (isFadingOut)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
+            Transform spriteObjectTrans = transform.GetChild(0);
+            GameObject spriteObject = spriteObjectTrans.gameObject;
+            SpriteRenderer sr = spriteObject.GetComponent<SpriteRenderer>();
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, (float)(sr.color.a - 0.05));
+            fadingOutTime++;
+            if (fadingOutTime > 30)
+            {
+                Destroy(transform.gameObject);
+            }
+        } else if (isFadingIn)
+        {
+            Transform spriteObjectTrans = transform.GetChild(0);
+            GameObject spriteObject = spriteObjectTrans.gameObject;
+            SpriteRenderer sr = spriteObject.GetComponent<SpriteRenderer>();
+            sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, (float)(sr.color.a + 0.02));
+            if (sr.color.a >= 1.0f)
+            {
+                isFadingIn = false;
+            }
+
+        }
+	}
+
+    public void FadeOutTextBox()
+    {
+        isFadingOut = true;
+        isFadingIn = false;
+        fadingOutTime = 0;
+    }
+
+    public void FadeInTextBox()
+    {
+        isFadingIn = true;
+        
+    }
+
+    public void SetInvisible()
+    {
+        Transform spriteObjectTrans = transform.GetChild(0);
+        GameObject spriteObject = spriteObjectTrans.gameObject;
+        SpriteRenderer sr = spriteObject.GetComponent<SpriteRenderer>();
+        sr.color = new Color(sr.color.r, sr.color.g, sr.color.b, 0);
+    }
+}
