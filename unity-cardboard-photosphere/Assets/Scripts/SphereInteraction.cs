aba70cf8d709e739bf70a883dd536bb6ebc39b29
maxahn <ahnmaximus@gmail.com>
2016-11-20 19:52:32 -0800

    interaction fixed

--- unity-cardboard-photosphere/Assets/Scripts/SphereInteraction.cs
+++ unity-cardboard-photosphere/Assets/Scripts/SphereInteraction.cs
@@ -4,7 +4,6 @@ using System.Collections;
 public class SphereInteraction : MonoBehaviour {
 
   // Use this for initialization}
-  public Camera camera;
 
   private bool inSphere = false;
 
@@ -13,31 +12,20 @@ public class SphereInteraction : MonoBehaviour {
   }
 
   void Update() {
-    if (inSphere && Input.GetMouseButtonDown (0)) {
-      camera.transform.position = new Vector3 (0, 10, 0); 
-      inSphere = false;
-    }
   }
 
   public void LookAtSphere() {
     if (!inSphere)
-      Debug.Log ("looking at sphere!");
       setHalo (true);
   }
   public void LookAwaySphere() {
     if (!inSphere)
-      Debug.Log ("looking away from sphere!");
       setHalo (false);
     }
 
   private void setHalo(bool active) {
     Component halo = GetComponent ("Halo");
     halo.GetType().GetProperty("enabled").SetValue(halo, active, null);
-  }
-
-  public void MoveCameraToSphere() {
-    camera.transform.position = transform.position;
-    inSphere = true;
   }
 }
 
