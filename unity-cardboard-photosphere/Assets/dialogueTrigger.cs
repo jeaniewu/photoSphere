6e1b7b795904df49c16ed542ba363ce98d35cbc5
Jeanie Suparman <jeanie.wu@hotmail.com>
2016-11-24 18:52:54 -0800

    all resources added

---
+++ unity-cardboard-photosphere/Assets/dialogueTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dialogueTrigger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GvrViewer.Instance.Triggered && this.gameObject.activeSelf) {
+			this.gameObject.SetActive (false);
+		}
+	}
+}
