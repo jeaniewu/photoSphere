35c4726e64a810d76c1e14f9260cf578f76134d7
Michael Goh <mike_goh88@hotmail.com>
2017-07-28 11:34:33 -0700

    Update
    - combined all groups into one full Demo fair

---
+++ unity-cardboard-photosphere/Assets/MoveGroups.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveGroups : MonoBehaviour {
+	
+	void Start(){
+
+	}
+	// Update is called once per frame
+	public void ChangeScene (Collider other) {
+		SceneManager.LoadScene (other.tag);
+
+		if(other.CompareTag("Sphere"))
+			SceneManager.LoadScene("DemoFair");
+	}
+}
